79e7e1353f000260fbad103347f6002a0b5f86fa
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 23:10:57 +0100

    furbo acabao

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,27 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+/***
+ * @author  José Miguel Ferreira Díaz
+ */
 public class Main {
 
-    static List<String> listadoEquipos, listadoCuartos, listadoSemifinal;
+    static List<String> listadoEquipos, listadoOctavos = new ArrayList<>(1), listadoCuartos = new ArrayList<>(1),
+            listadoSemiFinal = new ArrayList<>(1), listadoFinal = new ArrayList<>(1);
 
     static int golesA,golesB;
+
+    static String equipoA, equipoB;
+
+    static boolean octavosFinalizado = false, cuartosFinalizado = false, semiFinalFinalizada = false, finalFinalizada = false;
+
     public static void main(String[] args) {
         try{
             //Conexión BBDD
             ManagerBBDD.conectar();
 
             //Borrar tablas
-            ManagerBBDD.borrarTablas();
+            ManagerBBDD.EliminarTablas();
             //region Crear tablas
             String []camposEquipo = {"equipo varchar(25)", "ganados int", "empatados int", "perdidos int",
             "golesMarcados int", "golesRecibidos int"};
@@ -30,47 +39,132 @@ public class Main {
             String []campos = {"equipoA varchar(25)", "equipoB varchar(25)", "golesA int", "golesB int"};
             ManagerBBDD.crearTabla("Octavos", campos);
             ManagerBBDD.crearTabla("Cuartos", campos);
+            ManagerBBDD.crearTabla("Semifinales", campos);
             ManagerBBDD.crearTabla("Final", campos);
             //endregion
 
             //Insertar los equipos en la tabla equipos
             insertarEquiposListado();
 
-
-            int opc = menu();
-                 switch (opc) {
-                     case 1:
-                         sacarEquipoAleatorio(listadoEquipos);
-                         break;
-                     case 2:
-                         break;
-                     case 3:
-                         break;
-                     case 4:
-                         break;
-                     case 5:
-                         break;
-                     case 6:
-                         break;
-                     default:
-                 }
-
-
-            //region Octavos
-            leerEquiposFicheros();
-
-            //Se asignan el equipo A y B
-            if (listadoEquipos.size() >= 2){
-                String equipoA = sacarEquipoAleatorio();
-                String equipoB = sacarEquipoAleatorio();
-                System.out.println(equipoA+" VS "+equipoB);
-                partido(equipoA, equipoB);
-                String [] columnasOctavos = {"equipoA", "equipoB", "golesA", "golesB"};
-                String [] columnasValoresOctavos = {equipoA, equipoB, Integer.toString(golesA), Integer.toString(golesB)};
-                ManagerBBDD.insertarTablas("Octavos", columnasOctavos, columnasValoresOctavos);
-                ManagerBBDD.mostrarSelect("Octavos",columnasOctavos);
+            //Hacemos un sorteo para ver que 16 equipos van a octavos
+            while(listadoOctavos.size() < 16){
+                listadoOctavos.add(sacarEquipoAleatorio(listadoEquipos));
             }
-            //endregion
+
+            while(true){
+                /***
+                 * Los partidos se jugaran 1 por 1, es decir el usuario deberá pulsar 8 veces la opción de octavos hasta que los 16 equipos hayán jugado
+                 * e igual con las demás opciones del menú. No sé si lo querias así o con un while dentro del menú donde se jueguen todos los partidos de golpe.
+                 */
+                int opc = menu();
+                switch (opc) {
+                    case 1:
+                        if (listadoOctavos.size() == 0){
+                            octavosFinalizado = true;
+                        }
+                        if (listadoOctavos.size() >= 2 && !octavosFinalizado){
+                            //Empieza el partido
+                            partido(listadoOctavos);
+                            if (golesA > golesB){
+                                //Añadimos al equipo ganador al listado de los cuartos.
+                                listadoCuartos.add(equipoA);
+                            } else {
+                                //Añadimos al equipo ganador al listado de los cuartos.
+                                listadoCuartos.add(equipoB);
+                            }
+                            insertarResultados("Octavos");
+                        } else {
+                            ManagerBBDD.listarTabla("Octavos");
+                        }
+                        break;
+                    case 2:
+                        if(octavosFinalizado){
+                            if (listadoCuartos.size() == 0){
+                                cuartosFinalizado = true;
+                            }
+                            if (listadoCuartos.size() >= 2 && !cuartosFinalizado){
+                                partido(listadoCuartos);
+                                if (golesA > golesB){
+                                    //Añadimos al equipo ganador al listado de la semifinal.
+                                    listadoSemiFinal.add(equipoA);
+                                } else {
+                                    //Añadimos al equipo ganador al listado de los semifinal.
+                                    listadoSemiFinal.add(equipoB);
+                                }
+                                insertarResultados("Cuartos");
+                            } else {
+                                ManagerBBDD.listarTabla("Cuartos");
+                            }
+                        } else {
+                            System.out.println("Todavía no se han acabado los Octavos");
+                        }
+
+                        break;
+                    case 3:
+                        if (cuartosFinalizado){
+                            if (listadoSemiFinal.size() == 0){
+                                semiFinalFinalizada = true;
+                            }
+                            if (listadoSemiFinal.size() >= 2 && !semiFinalFinalizada){
+                                partido(listadoSemiFinal);
+                                if (golesA > golesB){
+                                    //Añadimos al equipo ganador al listado de la final.
+                                    listadoFinal.add(equipoA);
+                                } else {
+                                    //Añadimos al equipo ganador al listado de la final.
+                                    listadoFinal.add(equipoB);
+                                }
+                                insertarResultados("Semifinales");
+                            } else {
+                                ManagerBBDD.listarTabla("Semifinales");
+                            }
+                        }else {
+                            System.out.println("Todavía no se han acabado los cuartos");
+                        }
+                        break;
+                    case 4:
+                        if (semiFinalFinalizada){
+                            if (listadoFinal.size() >= 2 && !finalFinalizada){
+                                partido(listadoFinal);
+                                finalFinalizada = true;
+                                insertarResultados("Final");
+                            } else {
+                                ManagerBBDD.listarTabla("Final");
+                            }
+                        }else {
+                            System.out.println("Todavía no se ha acabado la Semifinal");
+                        }
+                        break;
+                    case 5:
+                        //Borramos todo el contenido de las tablas
+                        ManagerBBDD.borrarTabla("Equipos", "");
+                        ManagerBBDD.borrarTabla("Octavos", "");
+                        ManagerBBDD.borrarTabla("Cuartos", "");
+                        ManagerBBDD.borrarTabla("Semifinales", "");
+                        ManagerBBDD.borrarTabla("Final", "");
+                        //Volvemos a meter los listados del fichero en la tabla equipos
+                        insertarEquiposListado();
+
+                        //Reiniciamos los booleanos
+                        octavosFinalizado = false;
+                        cuartosFinalizado = false;
+                        semiFinalFinalizada = false;
+                        finalFinalizada = false;
+
+                        //Hacemos un sorteo para ver que 16 equipos van a octavos
+                        while(listadoOctavos.size() < 16){
+                            listadoOctavos.add(sacarEquipoAleatorio(listadoEquipos));
+                        }
+                        break;
+                    case 6:
+                        System.exit(0);
+                        break;
+                    default:
+                }
+            }
+
+
+
         }catch (SQLException e){
             e.printStackTrace();
         } catch (FileNotFoundException e) {
@@ -84,10 +178,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         // Imprimimos el menú con las diversas opciones
-        System.out.println("1. Iniciar Octavos");
-        System.out.println("2. Iniciar Cuartos");
-        System.out.println("3. Iniciar Semifinales");
-        System.out.println("4. Iniciar la Final");
+        if (!octavosFinalizado){
+            System.out.println("1. Iniciar Octavos");
+        } else {
+            System.out.println("1. Resultado Octavos");
+        }
+        if (!cuartosFinalizado){
+            System.out.println("2. Iniciar Cuartos");
+        } else {
+            System.out.println("1. Resultado Cuartos");
+        }
+        if (!semiFinalFinalizada){
+            System.out.println("3. Iniciar Semifinales");
+        } else {
+            System.out.println("1. Resultado Semifinal");
+        }
+        if (!finalFinalizada){
+            System.out.println("4. Iniciar la Final");
+        } else {
+            System.out.println("1. Resultado Final");
+        }
         System.out.println("5. Reiniciar");
         System.out.println("6. Salir");
 
@@ -99,7 +209,7 @@ public class Main {
     }
 
     /***
-     * Método que lee con un escaner los equipos del fichero y los mete en un ArrayLis<String>
+     * Método que lee con un escaner los equipos del fichero y los mete en un ArrayList<String>
      * @return
      * @throws FileNotFoundException
      */
@@ -138,25 +248,47 @@ public class Main {
      * Saca un equipo aleatorio del listado de equipos;
      * @return
      */
-    private static String sacarEquipoAleatorio(){
-        int myRand =  (int)(Math.random()*listadoEquipos.size());
-        String equipo = listadoEquipos.get(myRand);
-        listadoEquipos.remove(myRand);
-        System.out.println("Equipo aleatorio = "+equipo);
+    private static String sacarEquipoAleatorio(List<String> listado){
+        int myRand =  (int)(Math.random()*listado.size());
+        String equipo = listado.get(myRand);
+        listado.remove(myRand);
+        //System.out.println("Equipo aleatorio = "+equipo);
         return equipo;
     }
 
-    private static void partido(List<String> listado){
+    /***
+     * Método que inserta los resultados del partido en la BBDD
+     * @param tabla
+     * @throws SQLException
+     */
+    private static void insertarResultados(String tabla) throws SQLException {
+        String [] columnas = {"equipoA", "equipoB", "golesA", "golesB"};
+        String [] columnasValores = {equipoA, equipoB, Integer.toString(golesA), Integer.toString(golesB)};
+        ManagerBBDD.insertarTablas(tabla, columnas, columnasValores);
+        ManagerBBDD.mostrarSelect(tabla,columnas);
+    }
 
-        //sacamos dos equipos aleatorios
-        String equipoA = sacarEquipoAleatorio();
-        String equipoB = sacarEquipoAleatorio();
+    /***
+     * Método que ejecuta el algoritmo del partido entre los 2 equipos aleatorios extraidos del
+     * listado
+     * @param listado
+     */
+    private static void partido(List<String> listado) throws SQLException {
 
-        listado.remove();
+        //Reiniciamos los goles
+        golesA = 0;
+        golesB = 0;
+
+        //sacamos dos equipos aleatorios del listado pasado
+        equipoA = sacarEquipoAleatorio(listado);
+        equipoB = sacarEquipoAleatorio(listado);
+
+        System.out.println(equipoA+" VS "+equipoB);
 
         int posesionA = 50;
         int posesionB = 50;
         long start = System.currentTimeMillis();
+        //El partido dura 90 segundos
         long total = TimeUnit.SECONDS.toMillis(90);
         long remain;
         while ((remain = start + total - System.currentTimeMillis()) > 0 || golesA == 0 && golesB == 0 && golesA == golesB) {
@@ -190,6 +322,47 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+
+        //Actualizamos el resultado de los equipos en la tabla equipos
+        String []camposEquipo = {"ganados", "empatados", "perdidos",
+                "golesMarcados", "golesRecibidos"};
+        //Recogemos los datos del EquipoA
+        int ganadosA = ManagerBBDD.returnSelectTablaEquipo(new String[]{"ganados"}, equipoA);
+        int empatadosA = ManagerBBDD.returnSelectTablaEquipo(new String[]{"empatados"}, equipoA);
+        int perdidosA = ManagerBBDD.returnSelectTablaEquipo(new String[]{"perdidos"}, equipoA);
+        int golesMarcadosA = ManagerBBDD.returnSelectTablaEquipo(new String[]{"golesMarcados"}, equipoA);
+        int golesRecibidosA = ManagerBBDD.returnSelectTablaEquipo(new String[]{"golesRecibidos"}, equipoA);
+
+        //Recogemos los datos del EquipoB
+        int ganadosB = ManagerBBDD.returnSelectTablaEquipo(new String[]{"ganados"}, equipoB);
+        int empatadosB = ManagerBBDD.returnSelectTablaEquipo(new String[]{"empatados"}, equipoB);
+        int perdidosB = ManagerBBDD.returnSelectTablaEquipo(new String[]{"perdidos"}, equipoB);
+        int golesMarcadosB = ManagerBBDD.returnSelectTablaEquipo(new String[]{"golesMarcados"}, equipoB);
+        int golesRecibidosB = ManagerBBDD.returnSelectTablaEquipo(new String[]{"golesRecibidos"}, equipoB);
+
+        if (golesA > golesB){
+            System.out.println("Gana "+equipoA);
+            //Actualizamos el resultado del perdedor
+            String []camposEquipoValuesPerdedor = {String.valueOf(ganadosB), String.valueOf(empatadosB), String.valueOf(perdidosB + 1),
+                    String.valueOf(golesMarcadosB + golesB), String.valueOf(golesRecibidosB + golesA)};
+            ManagerBBDD.modificarTabla("Equipos",camposEquipo,camposEquipoValuesPerdedor,"equipo LIKE '"+equipoB+"'");
+
+            //Actualizamos el resultado del ganador
+            String []camposEquipoValuesGanador = {String.valueOf(ganadosA + 1), String.valueOf(empatadosA), String.valueOf(perdidosA),
+                    String.valueOf(golesMarcadosA + golesA), String.valueOf(golesRecibidosA + golesB)};
+            ManagerBBDD.modificarTabla("Equipos",camposEquipo,camposEquipoValuesGanador,"equipo LIKE '"+equipoA+"'");
+        } else {
+            System.out.println("Gana "+equipoB);
+            //Actualizamos el resultado del perdedor
+            String []camposEquipoValuesPerdedor = {String.valueOf(ganadosA), String.valueOf(empatadosA), String.valueOf(perdidosA + 1),
+                    String.valueOf(golesMarcadosA + golesA), String.valueOf(golesRecibidosA + golesB)};
+            ManagerBBDD.modificarTabla("Equipos",camposEquipo,camposEquipoValuesPerdedor,"equipo LIKE '"+equipoB+"'");
+
+            //Actualizamos el resultado del ganador
+            String []camposEquipoValuesGanador = {String.valueOf(ganadosB + 1), String.valueOf(empatadosB), String.valueOf(perdidosB),
+                    String.valueOf(golesMarcadosB + golesB), String.valueOf(golesRecibidosB + golesA)};
+            ManagerBBDD.modificarTabla("Equipos",camposEquipo,camposEquipoValuesGanador,"equipo LIKE '"+equipoA+"'");
         }
     }
 
--- src/ManagerBBDD.java
+++ src/ManagerBBDD.java
@@ -24,6 +24,12 @@ public class ManagerBBDD {
         }
     }
 
+    /***
+     * Crea las tablas con los datos pasados como parámetros
+     * @param tabla
+     * @param campos
+     * @throws SQLException
+     */
     public static void crearTabla(String tabla, String []campos) throws SQLException {
         String sql = "CREATE TABLE "+tabla+"(";
 
@@ -38,10 +44,47 @@ public class ManagerBBDD {
         st.executeUpdate(sql);
     }
 
-    public static void borrarTablas() throws SQLException {
-        st.executeUpdate("DROP TABLE Equipos, Octavos, Cuartos, Final");
+    /***
+     * Elimina las tablas con un DROP TABLE si existen
+     * @throws SQLException
+     */
+    public static void EliminarTablas() throws SQLException {
+        DatabaseMetaData dbm = connection.getMetaData();
+        //Comprobamos si la Tabla Final existe
+        ResultSet tables = dbm.getTables(null, null, "Final", null);
+        if (tables.next()) {
+            //Borramos las tablas
+            st.executeUpdate("DROP TABLE Equipos, Octavos, Cuartos, Semifinales, Final");
+        }
+        else {
+            System.out.println("No hay ninguna tabla creada todavía");
+        }
     }
 
+    /**
+     * Se borran los datos de la tabla cuando se da la condición, si no se borra todo.
+     * Después muestra la tabla
+     * @param tabla
+     * @param condicion
+     * @throws SQLException
+     */
+    public static void borrarTabla(String tabla, String condicion) throws SQLException{
+        String sql;
+        if (condicion != ""){
+            sql = "DELETE FROM "+tabla+" WHERE "+condicion;
+        } else {
+            sql = "DELETE FROM "+tabla;
+        }
+        st.executeUpdate(sql);
+    }
+
+    /***
+     * Se insertan los datos pasados como parámetros en las tablas
+     * @param tabla
+     * @param campos
+     * @param camposValores
+     * @throws SQLException
+     */
     public static void insertarTablas(String tabla, String[] campos, String[] camposValores) throws SQLException {
         String sql = "INSERT INTO "+tabla+"(";
 
@@ -62,6 +105,30 @@ public class ManagerBBDD {
             }
         }
         sql += ")";
+        st.executeUpdate(sql);
+    }
+
+    /**
+     * Modifica las columnas de la tabla pasadas en el array campos
+     * con los nuevos valores del array valores cuando se da la condición.
+     * Después muestra la tabla
+     * @param tabla
+     * @param campos
+     * @param valores
+     * @param condicion
+     * @throws SQLException
+     */
+    public static void modificarTabla(String tabla, String[]campos, String[]valores, String condicion) throws SQLException{
+        String sql = "UPDATE "+tabla+" SET ";
+
+        for(int i = 0; i < campos.length; i++){
+            if (i == campos.length - 1){
+                sql += campos[i]+" = "+valores[i];
+            } else {
+                sql += campos[i] +" = "+valores[i]+", ";
+            }
+        }
+        sql+= " WHERE "+condicion;
         st.executeUpdate(sql);
     }
 
@@ -87,6 +154,12 @@ public class ManagerBBDD {
         System.out.println();
     }
 
+    /***
+     * Imprime en la consola el SELECT con las columnas pasadas como parámetros
+     * @param tabla
+     * @param columnas
+     * @throws SQLException
+     */
     public static void mostrarSelect (String tabla, String[] columnas) throws SQLException{
         String sql = "SELECT * FROM "+tabla;
         ResultSet rs;
@@ -98,5 +171,27 @@ public class ManagerBBDD {
             }
             System.out.println();
         }
+    }
+
+    /***
+     * Devuelve los datos deseados de la tabla Equipos
+     * @param columnas
+     * @param equipo
+     * @return
+     * @throws SQLException
+     */
+    public static int returnSelectTablaEquipo (String[] columnas, String equipo) throws SQLException{
+        String sql = "SELECT * FROM Equipos WHERE equipo LIKE '"+equipo+"'";
+        ResultSet rs;
+        String resultado = null;
+        rs = st.executeQuery(sql);
+        while(rs.next()) {
+            for (String columna: columnas
+            ) {
+                resultado = rs.getString(columna);
+            }
+            System.out.println();
+        }
+        return Integer.parseInt(resultado);
     }
 }
